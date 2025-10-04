c713ce6e83ae4afb5b542a83a980a246f4acb801
ZinC0508 <18335432463@163.com>
2025-10-04 18:30:57 +0800

    Create AttributeManager

--- AttributesManager.cs
+++ AttributesManager.cs
@@ -1,8 +1,19 @@
 using Godot;
 using System;
+using Godot.Collections;
 
 public partial class AttributesManager : Node
 {
     public static float MaxHealth = 100;
     public static float Speed = 300f;
+
+    public static void SaveAttributes()
+    {
+        var dic = new Dictionary<string, float>();
+        dic.Add("MaxHealth",MaxHealth);
+    }
+    public static void LoadAttributes()
+    {
+        
+    }
 }
