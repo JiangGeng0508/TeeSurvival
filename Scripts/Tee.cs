fd7680e09a1d428f0259dde5b55d1eb44b25a232
ZinC0508 <18335432463@163.com>
2025-10-07 15:13:02 +0800

    awful hook

--- Scripts/Tee.cs
+++ Scripts/Tee.cs
@@ -28,6 +28,8 @@ public partial class Tee : CharacterBody2D
 	private bool _dodgeFinished = true;
 	private float _axis = 0f;
 	private Vector2 _homePosition = Vector2.Zero;
+	private Vector2 _hookGlobalPosition = Vector2.Zero;
+	private bool _isHooking = false;
 	public override void _Ready()
 	{
 		_stateLabel = GetNode<Label>("StateLabel");
@@ -79,9 +81,16 @@ public partial class Tee : CharacterBody2D
 
 		var move = new Vector2(_axis * Speed, 0);
 		//TODO:处理钩子以及勾子拉力
-		var hookForce = new Vector2();
+		//射线检测
+		var hookForce = Vector2.Zero;
+		_hookRay.TargetPosition = GetLocalMousePosition().Normalized() * 300f;
+		if (_isHooking)
+		{
+			hookForce = (_hookGlobalPosition - GlobalPosition) * 5f;
+			_hookLine.Points = [Vector2.Zero, _hookGlobalPosition - GlobalPosition];
+		}
 		//更新速度
-		Velocity = move + _exVelocity + _gravityAcc;
+		Velocity = move + _exVelocity + _gravityAcc + hookForce;
 		MoveAndSlide();
 		
 		if(Velocity.Y > 1000f) Velocity = new Vector2(Velocity.X,1000f);
@@ -92,7 +101,8 @@ public partial class Tee : CharacterBody2D
 		_teeSkin.EyesSprite.Position = GetLocalMousePosition().Normalized() * 3f;//控制眼睛偏移
 		//更新光标
 		_cursorLine.Points = [Vector2.Zero, GetLocalMousePosition().Normalized() * 300f];
-		_cursorMarker.Position = GetLocalMousePosition();
+		_cursorMarker.Position = _hookGlobalPosition - GlobalPosition;
+		// _cursorMarker.Position = GetLocalMousePosition();
 		//更新手节点位置和角度
 		_hand.Position = GetLocalMousePosition().Normalized() * 20f;
 		_hand.Rotation = GetLocalMousePosition().Angle();
@@ -102,59 +112,57 @@ public partial class Tee : CharacterBody2D
 		_axis = Input.GetAxis("move_left", "move_right");
 		if (@event is InputEventKey eventKey)
 		{
-			if (eventKey.Pressed)
+			if (eventKey.IsActionPressed("dodge") && _dodgeCount > 0 && Velocity.X != 0)
 			{
-				if (eventKey.IsActionPressed("jump") && _jumpCount > 0)
+				//按下“闪避”键时，改变X方向速度，并在0.1秒后复原
+				var acc = Velocity.X > 0 ? DodgeSpeed : -DodgeSpeed;
+				_exVelocity.X += acc;
+				GetTree().CreateTimer(0.1f, false).Timeout += () =>
 				{
-					//按下“跳跃”键时，改变Y方向速度，重置重力并减少跳跃次数，腾空标志改为true
-					_exVelocity += new Vector2(0, -JumpSpeed);
-					_gravityAcc = new Vector2(0, 0);
-					_jumpCount--;
-					_isJumping = true;
-				}
-				if (eventKey.IsActionPressed("dodge") && _dodgeCount > 0 && Velocity.X != 0)
-				{
-					//按下“闪避”键时，改变X方向速度，并在0.1秒后复原
-					var acc = Velocity.X > 0 ? DodgeSpeed : -DodgeSpeed;
-					_exVelocity.X += acc;
-					GetTree().CreateTimer(0.1f, false).Timeout += () =>
-					{
-						_exVelocity.X -= acc;
-						_dodgeFinished = true;
-					};
-					_dodgeCount--;
-				}
-				if (eventKey.IsActionPressed("respawn")) Respawn(eventKey.IsCtrlPressed());
+					_exVelocity.X -= acc;
+					_dodgeFinished = true;
+				};
+				_dodgeCount--;
 			}
-			else
-			{
-				if (eventKey.IsActionReleased("jump") && _isJumping)
-				{
-					//腾空并松开“跳跃”键时，重置Y方向速度和重力，腾空标志改为false
-					_exVelocity.Y = 100;
-					_isJumping = false;
-					_gravityAcc = new Vector2(0, 0);
-				}
 
-				
-			}
-			SetFoot(!(Mathf.Abs(Velocity.X) > 10));
-		}
-
-		if (@event is InputEventMouseButton eventMouseButton)
-		{
-			if (eventMouseButton.IsActionPressed("hook"))
+			if (eventKey.IsActionPressed("respawn"))
+				Respawn(eventKey.IsCtrlPressed());
+			if (eventKey.IsActionPressed("jump") && _jumpCount > 0)
 			{
-				_hookRay.TargetPosition = GetLocalMousePosition().Normalized() * 300f;
-				var pos = _hookRay.GetCollisionPoint();
-				_hookLine.Points = [Vector2.Zero, pos - GlobalPosition];
+				//按下“跳跃”键时，改变Y方向速度，重置重力并减少跳跃次数，腾空标志改为true
+				_exVelocity += new Vector2(0, -JumpSpeed);
+				_gravityAcc = new Vector2(0, 0);
+				_jumpCount--;
+				_isJumping = true;
 			}
-			else if (eventMouseButton.IsActionReleased("hook"))
+			else if (eventKey.IsActionReleased("jump") && _isJumping)
 			{
-				_hookLine.Points = [Vector2.Zero, Vector2.Zero];
+				//腾空并松开“跳跃”键时，重置Y方向速度和重力，腾空标志改为false
+				_exVelocity.Y = 100;
+				_isJumping = false;
+				_gravityAcc = new Vector2(0, 0);
 			}
 			
 		}
+		if (@event.IsActionPressed("hook"))
+		{
+			if (!_hookRay.CollideWithBodies)
+			{
+				_isHooking = false;
+				_hookLine.Hide();
+				return;
+			}
+			var pos = _hookRay.GetCollisionPoint();
+			_hookGlobalPosition = pos;
+			_isHooking = true;
+			_hookLine.Show();
+		}
+		else if (@event.IsActionReleased("hook"))
+		{
+			_isHooking = false;
+			_hookLine.Hide();
+		}
+		SetFoot(!(Mathf.Abs(Velocity.X) > 10));
 	}
 	private void Respawn(bool reload = false)
 	{
